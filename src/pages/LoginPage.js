15f902598b18c177be4cc9fbc269fb6d1f316389
Vinzh <kevin.zhong@gmail.com>
2025-01-24 13:55:46 +0700

    delui whistleblower

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,7 +3,7 @@ import { Form } from 'antd';
 export default function LoginPage() {
     return <div>
         <Form>
-            <Form.Item>
+            <Form.Item key='login' name='login' label='로그인 Login' rules={[{ warningOnly: true, message: '로그인 Login' }]}> 
                 <p>로그인 Login</p>
             </Form.Item>
         </Form>
---
+++ src/pages/deluiWhitler.js
@@ -0,0 +1,47 @@
+class Shape {
+    constructor(name) {
+        this.name = name;
+    }
+
+    area() {
+        throw new Error("Method 'area()' must be implemented.");
+    }
+
+    toString() {
+        return `${this.name} with area ${this.area()}`;
+    }
+}
+
+class Circle extends Shape {
+    constructor(radius) {
+        super('Circle');
+        this.radius = radius;
+    }
+
+    area() {
+        return Math.PI * this.radius * this.radius;
+    }
+}
+
+class Rectangle extends Shape {
+    constructor(width, height) {
+        super('Rectangle');
+        this.width = width;
+        this.height = height;
+    }
+
+    area() {
+        return this.width * this.height;
+    }
+}
+
+const shapes = [
+    new Circle(10),
+    new Rectangle(10, 50)
+];
+
+shapes.forEach(shape => {
+    console.log(shape.toString());
+});
+
+export { Circle, Rectangle };
