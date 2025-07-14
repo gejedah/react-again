29677206cddaeb81e19100852e7b615bb8b303dd
Vinzh <kevin.zhong@gmail.com>
2025-07-14 16:49:53 +0700

    update package

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,11 +1,44 @@
-import { Form } from 'antd';
+import { Form, Input, Button, Checkbox, message } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
 
 export default function LoginPage() {
-    return <div>
-        <Form>
-            <Form.Item key='login' name='login' label='SignIn' rules={[{ warningOnly: true, message: '로그인 Login' }]}> 
-                <p>로그인 Login</p>
-            </Form.Item>
-        </Form>
+    return    <div style={{ maxWidth: "300px", margin: "50px auto" }}>
+      <Form
+        name="basic"
+        initialValues={{ remember: true }}
+        // onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input prefix={<UserOutlined />} placeholder="아이디" />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password
+            prefix={<LockOutlined />}
+            type="password"
+            placeholder="비밀번호"
+          />
+        </Form.Item>
+
+        <Form.Item name="remember" valuePropName="checked">
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" block>
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
     </div>;
 }
